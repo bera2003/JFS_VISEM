a27c6cb5a28bd8515ba8c62f9dd0e9ab2a9e406e
bera2003 <anuragbera7@gmail.com>
2025-12-28 23:56:48 +0530

    Reverse string program using Maven

---
+++ Maven_Projects/reverse-string-maven/src/main/java/com/company/reversestring/main/ReverseStringApp.java
@@ -0,0 +1,28 @@
+package com.company.reversestring.main;
+
+import java.util.Scanner;
+
+public class ReverseStringApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        char[] chars = input.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        System.out.println("Reversed string: " + new String(chars));
+    }
+}
